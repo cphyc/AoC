d996c866fef3e39330f8be2b0238082a46685065
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-12-02 09:21:32 +0100

    Solve day two, part II

--- 2022/day2.py
+++ 2022/day2.py
@@ -1,46 +1,48 @@
 import typing
 from pathlib import Path
 
-ROCK = 1
-PAPER = 2
-SCISSORS = 3
 
-
-def read_input() -> typing.Generator[tuple[int, int], None, None]:
+def read_input() -> typing.Generator[tuple[str, str], None, None]:
     with open(Path(__file__).parent / "input2") as f:
-        lines = (_.strip() for _ in f.readlines())
-
-    for line in lines:
-        match line[0]:
-            case "A":
-                theirs = ROCK
-            case "B":
-                theirs = PAPER
-            case "C":
-                theirs = SCISSORS
-        match line[2]:
-            case "X":
-                ours = ROCK
-            case "Y":
-                ours = PAPER
-            case "Z":
-                ours = SCISSORS
-        yield theirs, ours
+        while line := f.readline():
+            yield line.strip().split()
 
 
 def part1():
     score = 0
     for theirs, ours in read_input():
-        score += ours
+        score += dict(X=1, Y=2, Z=3)[ours]
         match (theirs, ours):
-            # We win
-            case (1, 2) | (2, 3) | (3, 1):
+            case ("A", "Y") | ("B", "Z") | ("C", "X"):
                 score += 6
-            case (a, b) if a == b:
+            case ("A", "X") | ("B", "Y") | ("C", "Z"):
                 score += 3
             case _:
                 score += 0
     print("Day 2, Part I:", score)
 
 
+def part2():
+    score = 0
+    lose_map = dict(A="Z", B="X", C="Y")
+    win_map = dict(A="Y", B="Z", C="X")
+    tie_map = dict(A="X", B="Y", C="Z")
+    score_map = dict(X=1, Y=2, Z=3)
+    for theirs, ours in read_input():
+        if ours == "X":  # we need to lose
+            score += 0
+            will_play = lose_map[theirs]
+        elif ours == "Y":  # we need to tie
+            score += 3
+            will_play = tie_map[theirs]
+        elif ours == "Z":  # we need to win
+            score += 6
+            will_play = win_map[theirs]
+
+        score += score_map[will_play]
+
+    print("Day 2, Part II:", score)
+
+
 part1()
+part2()
