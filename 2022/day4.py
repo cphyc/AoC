76cc5d52e0c3800436528a2e4448ba0100a60df6
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-12-04 11:15:03 +0100

    Solve day four, Part II

--- 2022/day4.py
+++ 2022/day4.py
@@ -11,6 +11,13 @@ class Span:
     def __contains__(self, other: "Span") -> bool:
         return self.left <= other.left and self.right >= other.right
 
+    @staticmethod
+    def overlap(first: "Span", second: "Span") -> bool:
+        return bool(
+            set(range(first.left, first.right + 1))
+            & set(range(second.left, second.right + 1))
+        )
+
 
 def read_input() -> Generator[tuple[Span, Span], None, None]:
     with (Path(__file__).parent / "input4").open() as f:
@@ -29,4 +36,13 @@ def part1():
     print("Day 4, Part I:", Noverlap)
 
 
+def part2():
+    Noverlap = 0
+    for left, right in read_input():
+        if Span.overlap(left, right):
+            Noverlap += 1
+    print("Day 4, Part II:", Noverlap)
+
+
 part1()
+part2()
