3a226ac88d08e4fd1adf51809502eceb8d4ad44d
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-12-06 08:11:24 +0100

    Solve day six, Part I

---
+++ 2022/day6.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from pathlib import Path
+
+from more_itertools import windowed
+
+
+def read_input() -> str:
+    with open(Path(__file__).parent / "input6") as f:
+        return f.readline().strip()
+
+
+def part1():
+    buffer = read_input()
+    for i, subset in enumerate(windowed(buffer, 4)):
+        if Counter(subset).most_common(1)[0][1] == 1:
+            break
+    print("Day 6, part I:", i + 4)
+
+
+part1()
