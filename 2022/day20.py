81614eb3546a39aa41cbcf3408bef4243308264e
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-12-22 18:43:41 +0100

    Solving day 20, Part II

--- 2022/day20.py
+++ 2022/day20.py
@@ -16,23 +16,40 @@ raw_input = (Path(__file__).parent / "input20").read_text()
 values = [(i, int(line)) for i, line in enumerate(raw_input.splitlines())]
 
 
-def decrypt(encrypted: list[tuple[int, int]]):
+def decrypt(encrypted: list[tuple[int, int]], rounds: int = 1):
     # Work on a copy
     decrypted = copy(encrypted)
     N = len(decrypted)
 
-    for ind_val in encrypted:
-        _, val = ind_val
-        index = decrypted.index(ind_val)
-        # Remove the index from the list
-        decrypted.pop(index)
-        # and insert it at the correct position
-        decrypted.insert((index + val) % (N - 1), ind_val)
+    for _ in range(rounds):
+        for ind_val in encrypted:
+            _, val = ind_val
+            index = decrypted.index(ind_val)
+            # Remove the index from the list
+            decrypted.pop(index)
+            # and insert it at the correct position
+            decrypted.insert((index + val) % (N - 1), ind_val)
 
     return decrypted
 
 
-decrypted = np.array(decrypt(values))[:, 1]
-i0 = np.argwhere(decrypted == 0)[0, 0]
+def part1():
+    decrypted = np.array(decrypt(values))[:, 1]
+    i0 = np.argwhere(decrypted == 0)[0, 0]
 
-print("Part 1:", tmp := np.roll(decrypted, -i0)[1000:3001:1000], tmp.sum())
+    ipos = (i0 + np.array([1000, 2000, 3000])) % len(decrypted)
+    print("Part 1:", tmp := decrypted[ipos], tmp.sum())
+
+
+def part2():
+    encrytion_key = 811589153
+    encrypted_with_key = [(i, val * encrytion_key) for i, val in values]
+    decrypted = np.array(decrypt(encrypted_with_key, rounds=10))[:, 1]
+    i0 = np.argwhere(decrypted == 0)[0, 0]
+
+    ipos = (i0 + np.array([1000, 2000, 3000])) % len(decrypted)
+    print("Part 2:", tmp := decrypted[ipos], tmp.sum())
+
+
+part1()
+part2()
